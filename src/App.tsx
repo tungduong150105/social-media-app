3534ac87854345fd42d5d6f94d5a62c21775b3c4
Cao Tung Duong <tungduong@Caos-MacBook-Pro.local>
2025-08-20 10:50:38 +0700

    Config routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './styles/globals.css'
 
 const App = () => {
-  return <div className="text-gray-100">App</div>
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Route */}
+        {/* Private Route */}
+      </Routes>
+    </main>
+  )
 }
 
 export default App
